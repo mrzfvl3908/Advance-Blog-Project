b5e5cce13a6b911ef06943416ce2a662ebd70d88
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-01 15:44:26 +0330

    add post_detail in api blog_app

--- core/blog_app/api/v1/urls.py
+++ core/blog_app/api/v1/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'api-v1'
 urlpatterns = [
     path('post/', views.postList, name='post-list'),
+    path('post/<slug:slug>/', views.postDetail, name='post-detail'),
 ]
--- core/blog_app/api/v1/views.py
+++ core/blog_app/api/v1/views.py
@@ -4,3 +4,7 @@ from rest_framework.response import Response
 @api_view()
 def postList(request):
     return Response('OK')
+
+@api_view()
+def postDetail(request,slug):
+    return Response(slug)
