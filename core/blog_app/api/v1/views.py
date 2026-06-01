555f55140e408f36c27c7a49d1c148eebeb0a284
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-01 21:51:58 +0330

    add serializers and edit views.py in api/v1

--- core/blog_app/api/v1/views.py
+++ core/blog_app/api/v1/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from ...models import Post
 
 @api_view()
 def postList(request):
