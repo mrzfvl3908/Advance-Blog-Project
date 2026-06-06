98e203e63d482303097863684ca3336abfb150f4
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-06 21:24:04 +0330

    add serializermethodfield in serializer.py

--- core/blog_app/api/v1/serializers.py
+++ core/blog_app/api/v1/serializers.py
@@ -1,15 +1,48 @@
+from django.template.context_processors import request
 from rest_framework import serializers
 from blog_app.models import Post, Category
 
 
-# class PostSerializer(serializers.Serializer):
-#     id = serializers.IntegerField()
-#     title = serializers.CharField(max_length=300)
+class PostSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=300)
 
 class PostSerializer(serializers.ModelSerializer):
+    # content = serializers.ReadOnlyField()
+    snippet = serializers.ReadOnlyField(source='get_snippet')  # => ارتباط گیری با فانکشن نوشته شده در مدل
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'content', 'published_date', 'status']
+        fields = ['id', 'author', 'title', 'content', 'published_date', 'status', 'snippet', 'absolute_url']
+
+        # read_only_fields = ['content']
+
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+
+        if request:
+            return request.build_absolute_uri(
+                obj.get_absolute_url()
+            )
+
+        return obj.get_absolute_url()
+
+
+# class PostSerializer(serializers.HyperlinkedModelSerializer): راه حرفه ای تر برای ننوشتن متد پست برای نمایش url
+#     class Meta:
+#         model = Post
+#         fields = (
+#             'url',
+#             'id',
+#             'title',
+#             'content',
+#         )
+#
+#     url = serializers.HyperlinkedIdentityField(
+#         view_name='post-detail',
+#         lookup_field='slug'
+#     )
 
 
 class CategorySerializer(serializers.ModelSerializer):
--- core/blog_app/models.py
+++ core/blog_app/models.py
@@ -35,6 +35,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_snippet(self):
+        return self.content[0:5]
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
