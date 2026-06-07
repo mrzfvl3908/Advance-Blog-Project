8ce19951d326faeec367120f68d9cb14a7d52439
Mohammadreza <mrzfvl3908@gmail.com>
2026-06-07 08:56:28 +0330

    Update serializers.py

--- core/blog_app/api/v1/serializers.py
+++ core/blog_app/api/v1/serializers.py
@@ -3,9 +3,9 @@ from rest_framework import serializers
 from blog_app.models import Post, Category
 
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=300)
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=300)
 
 class PostSerializer(serializers.ModelSerializer):
     # content = serializers.ReadOnlyField()
